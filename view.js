1d8a306d91b25b8ad892454dbc000e4012c0273b
Joel Gimbel <joel@liveby.co>
2015-12-16 14:42:56 -0600

    remvoe return

--- view.js
+++ view.js
@@ -6,7 +6,7 @@ function View(name, options){
         this.root = options.root
 	this.defaultEngine = options.defaultEngine
 	this.engines = options.engines
-	return options
+//	return options
 }
 View.prototype.render = function(options, callback){
 	this.defaultEngine(this.path, options, callback)
