514aacc92b2f8c0c8929fa325e3830a30ecc31f8
Joel Gimbel <joel@liveby.co>
2015-12-16 14:46:37 -0600

    Object assing this

--- view.js
+++ view.js
@@ -2,10 +2,11 @@ var path = require('path')
 function View(name, options){
 	
 	options.path = path.join(options.root, name)
-	this.path = options.path
-        this.root = options.root
-	this.defaultEngine = options.defaultEngine
-	this.engines = options.engines
+	//this.path = options.path
+        //this.root = options.root
+	//this.defaultEngine = options.defaultEngine
+	//this.engines = options.engines
+	Object.assign(this, options)
 //	return options
 }
 View.prototype.render = function(options, callback){
