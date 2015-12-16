bb9f81d249d4bb0584927123b9a89c491df84a64
Joel Gimbel <joel@liveby.co>
2015-12-16 14:27:07 -0600

    adding render method

--- view.js
+++ view.js
@@ -2,6 +2,12 @@ var path = require('path')
 module.exports = function(name, options){
 	
 	options.path = path.join(options.root, name)
-	this = options
+	this.path = options.path
+        this.root = options.root
+	this.defaultEngine = options.defaultEngine
+	this.engines = options.engines
 	return options
+}
+module.exports.prototype.render = function(options, callback){
+	this.defaultEngine(this.path, options, callback)
 }
