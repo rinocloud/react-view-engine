915c8a4476193730f5b78c35dbe87926b82dd680
Joel Gimbel <jgimbel@live.com>
2016-04-19 12:38:47 -0500

    restructure and clean up

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 
 var React = require('react')
 var ReactDOMServer = require('react-dom/server')
-var inject = require('connect-inject')
 var path = require('path')
-var fs = require('fs')
-var layout
-var View = require('./view')
 
-function getComponent(file){
+function getComponent(file) {
     var r = require(file);
-    if(r.__esModule && r.default){
+    if (r.__esModule && r.default) {
         return r.default
     } else {
         return r
     }
 }
 
-function cleanOptions(options){
+function cleanOptions(options) {
     var opts = options
     delete opts['settings']
     delete opts['_locals']
@@ -27,55 +23,51 @@ function cleanOptions(options){
 }
 
 
-function handler(html, name, options){
+function handler(html, name, options) {
     var script = [
-        '<script type="application/json" id="props_'+name+'">',
-            JSON.stringify(options),
+        '<script type="application/json" id="props_' + name + '">',
+        JSON.stringify(options),
         '</script>',
         '<script>',
         "window.addEventListener('load', function(){",
-        "    var props = JSON.parse(document.getElementById('props_" + name + "').innerHTML)",
-        "    loadProps('" + name + "', '"  + layout + "', props)",
+        "  var props = JSON.parse(document.getElementById('props_" + name + "').innerHTML)",
+        "  loadProps('" + name + "', '" + layout + "', props)",
         "})",
         '</script>'
     ].join('\n')
     return html.replace('</body>', script + '</body>')
-    
+
 }
 
-exports = module.exports = loader = {
-    setLayout: function(l) { layout = l },
-    list : [],
-    view: View,
-    initJSX : function(){
-        return require('node-jsx').install();
-    },
-
-    engine : function (filePath, options, callback){
-        delete require.cache[require.resolve(filePath)]
-        
-        if(layout){
-            var layoutPath = this.lookup(layout+".jsx")
-            delete require.cache[require.resolve(layoutPath)]
-            var Layout = getComponent(layoutPath)
-        }
-        
-        var client = getComponent(filePath)
-        
-  	if(options.noHashes){
-		var render = ReactDOMServer.renderToStaticMarkup
-	} else {
-		var render = ReactDOMServer.renderToString
-	}
-
-        var name = this.name
-        var data = cleanOptions(options)
-        var clientApp = React.createFactory(client)(data)
-        var Template = Layout ? React.createFactory(Layout)(data, clientApp) : clientApp
-        var markup = render(Template)
-        markup = '<!DOCTYPE html>' + handler(markup, name, options)
-
-        return callback(null, markup)
+module.exports = function engine(l) {
+  var layout = l
+  require('babel-register')({
+    extensions: ['.jsx', '.js'],
+    sourceMaps: true
+  })
+  return function (filePath, options, callback) {
+    try {
+      delete require.cache[require.resolve(filePath)]
+      if (layout) {
+        var layoutPath = this.lookup(layout + ".jsx")
+        delete require.cache[require.resolve(layoutPath)]
+        var Layout = getComponent(layoutPath)
+      }
+      var client = getComponent(filePath)
+      if (options.noHashes) {
+        var render = ReactDOMServer.renderToStaticMarkup
+      } else {
+        var render = ReactDOMServer.renderToString
+      }
+      var name = this.name
+      var data = cleanOptions(options)
+      var clientApp = React.createFactory(client)(data)
+      var Template = Layout ? React.createFactory(Layout)(data, clientApp) : clientApp
+      var markup = render(Template)
+      markup = '<!DOCTYPE html>' + handler(markup, name, options)
+      return callback(null, markup)
+    }catch(error){
+      return callback(error)
     }
-
+  }
 }
